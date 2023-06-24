666cd7f1fa39381144864ba81a7e742c627cf5fb
Mferik <mfatiherikk@gmail.com>
2023-06-24 22:25:04 +0300

    tekli user getirme

--- api/index.js
+++ api/index.js
@@ -1,13 +1,43 @@
-const express = require("express")
+const express = require("express");
 
 const app = express();
 const port = 5000;
 
-app.get("/",(req,res) => {
-    res.send("Server aktif")
+let users = [
+  {
+    id: 1,
+    name: "fatih",
+    email: "cubake@gmail.com",
+    country: "turkey",
+    contact: "54545454",
+  },
+  {
+    id: 2,
+    name: "ebus",
+    email: "ebus@gmail.com",
+    country: "turkey",
+    contact: "321321",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("Server aktif");
+});
+
+app.get("/users",(req,res) => {
+    res.send(users)
 })
 
-
-app.listen(port,() => {
-    console.log(`Server is running on ${port}`)
+app.get("/users/:id",(req,res) => {
+    const id = req.params.id;
+    const user = users.find((user) => user.id ===Number(id))
+    if(!user){
+        res.status(400).send("Kullanıcı yok")
+    }
+    res.send(user)
+    console.log(id)
 })
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
