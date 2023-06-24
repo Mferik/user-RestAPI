8576566510d32d8be026615aac673100a60ee3b5
Mferik <mfatiherikk@gmail.com>
2023-06-24 22:36:49 +0300

    user put isteği

--- api/index.js
+++ api/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const bodyParser = require("body-parser")
-
-
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 const port = 5000;
 
 let users = [
@@ -43,23 +41,37 @@ app.get("/users/:id", (req, res) => {
   console.log(id);
 });
 
-app.post("/users",(req,res) => {
-    const {name,email,country,contact} = req.body
-    const user = {
-        id:users.length + 1,
-        name:name,
-        email:email,
-        country:country,
-        contact:contact
-    }
-    users.push(user)
-    res.send("Yeni kullanıcı oluşturuldu")
-})
+app.post("/users", (req, res) => {
+  const { name, email, country, contact } = req.body;
+  const user = {
+    id: users.length + 1,
+    name: name,
+    email: email,
+    country: country,
+    contact: contact,
+  };
+  users.push(user);
+  res.send("Yeni kullanıcı oluşturuldu");
+});
 
-app.delete("/users",(req,res) => {
-    users.filter((user) => user.id !== req.params.id)
-    res.send(users)
-})
+app.delete("/users", (req, res) => {
+  users.filter((user) => user.id !== req.params.id);
+  res.send(users);
+});
+
+app.put("/users:id", (req, res) => {
+  const id = req.params.id;
+  const user = users.find((user) => user.id === Number(id));
+  const { name, email, country, contact } = req.body;
+  if (!user) {
+    console.log("Kullanıcı yok");
+  }
+  (user.name = name),
+    (user.email = email),
+    (user.country = country),
+    (user.contact = contact);
+  res.send(user);
+});
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
